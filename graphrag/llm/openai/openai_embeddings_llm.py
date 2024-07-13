d7f60b93b413e3c580a782cf4500369ef72dd09e
paulg <paul.guo@iag.com.au>
2024-07-13 12:07:15 +1000

    ollama hacked

--- graphrag/llm/openai/openai_embeddings_llm.py
+++ graphrag/llm/openai/openai_embeddings_llm.py
@@ -15,6 +15,7 @@ from graphrag.llm.types import (
 from .openai_configuration import OpenAIConfiguration
 from .types import OpenAIClientTypes
 
+import ollama
 
 class OpenAIEmbeddingsLLM(BaseLLM[EmbeddingInput, EmbeddingOutput]):
     """A text-embedding generator LLM."""
@@ -33,8 +34,13 @@ class OpenAIEmbeddingsLLM(BaseLLM[EmbeddingInput, EmbeddingOutput]):
             "model": self.configuration.model,
             **(kwargs.get("model_parameters") or {}),
         }
-        embedding = await self.client.embeddings.create(
-            input=input,
-            **args,
-        )
-        return [d.embedding for d in embedding.data]
+        embedding_list = []
+        for inp in input:
+            embedding = ollama.embeddings(model="nomic-embed-text", prompt=inp)
+            embedding_list.append(embedding["embedding"])
+        return embedding_list
+        # embedding = await self.client.embeddings.create(
+        #     input=input,
+        #     **args,
+        # )
+        # return [d.embedding for d in embedding.data]
